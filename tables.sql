c5726704139e40c501d8de52cf44933852bbc54e
AliWaleed97 <ali.william67@gmail.com>
2019-04-25 19:53:02 +0200

    psql

--- tables.sql
+++ tables.sql
@@ -5,25 +5,25 @@
 	-- user_type VARCHAR (100) NOT NULL,
 	-- salt      VARCHAR(255) NOT NULL
 	-- );
-	CREATE TABLE Restaurants
-	(
-	id BIGSERIAL PRIMARY KEY NOT NULL,
-	username VARCHAR REFERENCES Users (username) ON DELETE CASCADE,
-	name VARCHAR (100) NOT NULL UNIQUE,
-	hotline VARCHAR (100),
-	delivery_time VARCHAR (100),
-	delivery_fees INT,
-	delivery_hours VARCHAR(200),
-	description text
-	);
+	-- CREATE TABLE Restaurants
+	-- (
+	-- id BIGSERIAL PRIMARY KEY NOT NULL,
+	-- username VARCHAR REFERENCES Users (username) ON DELETE CASCADE,
+	-- name VARCHAR (100) NOT NULL UNIQUE,
+	-- hotline VARCHAR (100),
+	-- delivery_time VARCHAR (100),
+	-- delivery_fees INT,
+	-- delivery_hours VARCHAR(200),
+	-- description text
+	-- );
 
-	CREATE TABLE Profiles
-	(
-	id BIGSERIAL PRIMARY KEY NOT NULL,
-	-- username VARCHAR REFERENCES "Users" (username) ON DELETE CASCADE,
-	name VARCHAR (100) NOT NULL UNIQUE,
-	birthdate VARCHAR(100) ,
-	bio VARCHAR(300),
-	phone_number VARCHAR(200),
-	address VARCHAR(200)
-	);
+	-- CREATE TABLE Profiles
+	-- (
+	-- id BIGSERIAL PRIMARY KEY NOT NULL,
+	-- -- username VARCHAR REFERENCES "Users" (username) ON DELETE CASCADE,
+	-- name VARCHAR (100) NOT NULL UNIQUE,
+	-- birthdate VARCHAR(100) ,
+	-- bio VARCHAR(300),
+	-- phone_number VARCHAR(200),
+	-- address VARCHAR(200)
+	-- );
