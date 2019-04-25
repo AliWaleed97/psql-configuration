250a10d4313267cd2c8a5996fccd9efbb45a11a2
AliWaleed97 <ali.william67@gmail.com>
2019-04-25 20:00:38 +0200

    psql

--- user_service/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users
-(id, user_name, password, user_type, salt) VALUES (1, 'ali','ali@hotmail.com','gibberish','0');
-
-INSERT INTO users
-(id, user_name, password, user_type, salt) VALUES (2, 'hazem','zoma@hotmail.com','gibberish', '0');
