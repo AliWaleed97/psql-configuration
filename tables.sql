4461e9f88ed2c55976558a84507c01f55e5efaad
AliWaleed97 <ali.william67@gmail.com>
2019-04-25 20:30:19 +0200

    psql

--- tables.sql
+++ tables.sql
@@ -5,17 +5,7 @@
 	user_type VARCHAR (100) NOT NULL,
 	salt      VARCHAR(255) NOT NULL
 	);
-	-- CREATE TABLE Restaurants
-	-- (
-	-- id BIGSERIAL PRIMARY KEY NOT NULL,
-	-- username VARCHAR REFERENCES Users (username) ON DELETE CASCADE,
-	-- name VARCHAR (100) NOT NULL UNIQUE,
-	-- hotline VARCHAR (100),
-	-- delivery_time VARCHAR (100),
-	-- delivery_fees INT,
-	-- delivery_hours VARCHAR(200),
-	-- description text
-	-- );
+
 
 	CREATE TABLE Profiles
 	(
