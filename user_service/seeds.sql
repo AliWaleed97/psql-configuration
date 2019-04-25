9c69443ee50d0c4bc1b96d542e9fa8e3bb43707c
AliWaleed97 <ali.william67@gmail.com>
2019-04-25 18:11:47 +0200

    psql

--- user_service/seeds.sql
+++ user_service/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO app_user
-(id, user_name, email, password, salt) VALUES (1, 'mahmooud','7oda@hotmail.com','gibberish','0');
+INSERT INTO users
+(id, user_name, password, user_type, salt) VALUES (1, 'ali','ali@hotmail.com','gibberish','0');
 
-INSERT INTO app_user
-(id, user_name, email, password, salt) VALUES (2, 'hazem','zoma@hotmail.com','gibberish', '0');
+INSERT INTO users
+(id, user_name, password, user_type, salt) VALUES (2, 'hazem','zoma@hotmail.com','gibberish', '0');
