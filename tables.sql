2b3a47c65958f8a2d08049f5fb574491919626b7
AliWaleed97 <ali.william67@gmail.com>
2019-04-25 20:53:40 +0200

    psql

--- tables.sql
+++ tables.sql
@@ -10,7 +10,7 @@
 	CREATE TABLE Profiles
 	(
 	id BIGSERIAL PRIMARY KEY NOT NULL,
-	-- username VARCHAR REFERENCES "Users" (username) ON DELETE CASCADE,
+	username VARCHAR REFERENCES "Users" (username) ON DELETE CASCADE,
 	name VARCHAR (100) NOT NULL UNIQUE,
 	birthdate VARCHAR(100) ,
 	bio VARCHAR(300),
